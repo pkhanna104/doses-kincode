c4803d032a817589e4fd19b47ea17b55d3629cf4
toppenheim1 <113718090+toppenheim1@users.noreply.github.com>
2022-09-28 12:03:18 -0700

    Add files via upload

--- tom_scripts/mean_rom_vs_sum_mse_total_2D.m
+++ tom_scripts/mean_rom_vs_sum_mse_total_2D.m
@@ -1,7 +1,7 @@
 clear;clc;close all;
 
-input = 'C9K';
-load 'C9K_pinch_data.mat';
+input = 'B8M';
+load 'B8M_pinch_data.mat';
 Filename = string(strcat('C:\Users\toppenheim\Desktop\UCSF\thumb data\Preeya Data\data\collated_data\Four healthy subject data\',input,'_jt_angle_un.mat'));
 load ([Filename]);
 plot_raw_data_v2(1,input,unaffected,angle_struct,1);hold on; 
@@ -137,272 +137,79 @@ for m = 1:12
         else
             color = col(3,:);
         end
-        
-%                figure(1)
-%                 if m < 6 
-%                     c = 1;
-%                     subplot(3,1,1)
-%                     plot(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),'s','Color',[col(sub,:)],'LineWidth', 2);hold on;
-%                     
-%                 elseif m > 6 & m < 10  
-%                     c = 2;
-%                     subplot(3,1,1)
-%                     plot(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),'h','Color',[col(sub,:)],'LineWidth', 2);hold on;
-%                     
-%                 else
-%                     c = 3;
-%                     subplot(3,1,1)
-%                     plot(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),'d','Color',[col(sub,:)],'LineWidth', 2);hold on;
-%                     
-%                 end
-%                  
-% %                scatter(rom(m,sub+1),mse(m,sub+1),[],ARAT(sub+1),'filled');hold on;
-% %                
-%                label = ["Thumb MCP","Thumb DIP","Index MCP","Index PIP","Index DIP","Elbow Flex","Palm Abd/Add","Palm Flex","Palm Sup/Pron","Shoulder Hor Flex","Shoulder Vert Flex","Shoulder Roll"];
-%                ylabel('Sum MSE of Z-Scored Data Norm (Aff-Un)/Un')
-%                xlabel('(Aff Std Ave - Un Std Ave) / Un Std Ave')
-%                zticks([1:3])
-%                zticklabels({'Distal Joints','Palm Joints','Proximal Joints'})
-%                axis([-1.5 8 -2 18])
-% 
-%                grid on
-               %axis([-1 1 0 15])
-               %legend('C9K Distal','B8M Distal','W16H Distal','S13J Distal','B12J Distal','R15J Distal',...
-                   %'C9K Palm','B8M Palm','W16H Palm','S13J Palm','B12J Palm','R15J Palm')
-                %legend
-                label = ["Thumb MCP","Thumb DIP","Index MCP","Index PIP","Index DIP","Elbow Flex","Palm Abd/Add","Palm Flex","Palm Sup/Pron","Shoulder Hor Flex","Shoulder Vert Flex","Shoulder Roll"];
 
-                if m < 6 
-                    c = 1;
-                    if rom(m,sub) < 1.5
-                        subplot(5,3,[10 11 13 14])
-%                         colormap jet
-%                         caxis([20 51])
-                    else
-                        subplot(5,3,[12 15])
-                        xlim([3 5.5])
-                    end
-                    %plot(u_rom_norm_mean(m,sub),u_mse_norm_mean(m,sub),'d','Color',[0 0 0],'LineWidth',4);hold on;
-                    e = errorbar(u_rom_norm_mean(m,sub),u_mse_norm_mean(m,sub),-u_mse_norm_std(m,sub),u_mse_norm_std(m,sub),-u_rom_norm_std(m,sub),u_rom_norm_std(m,sub),'LineWidth',2);hold on;
-                    e.Color = 'black';
-                    e.CapSize = 15;
-                    s = scatter(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),'filled','MarkerEdgeColor',color,'MarkerFaceColor',color);hold on;  
-                    s.SizeData = 100;
-                    s.Marker = 's';
-                    if m == 1
-                        text(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),label(m),'color',color,'fontweight','bold','FontSize', 8);
-                    end
-                    ylim([-2 10])
-                    
-                    %s = scatter(rom_2_pinch(m,sub),(mse_a_2_pinch(m,sub)-mse_u_2_pinch(m,sub))./mse_u_2_pinch(m,sub),[],'ks','filled');hold on;  
+        label = ["Thumb MCP","Thumb DIP","Index MCP","Index PIP","Index DIP","Elbow Flex","Palm Abd/Add","Palm Flex","Palm Sup/Pron","Shoulder Hor Flex","Shoulder Vert Flex","Shoulder Roll"];
 
-                elseif m > 6 && m < 10  
-                    c = 2;
-                   if rom(m,sub) < 1.5
-                        subplot(5,3,[10 11 13 14])
-%                         colormap jet
-%                         caxis([20 51])
-                    else
-                        subplot(5,3,[12 15])
-                    end
-                    %plot(u_rom_norm_mean(m,sub),u_mse_norm_mean(m,sub),'d','Color',[0 0 0],'LineWidth',4);hold on;
-                    e = errorbar(u_rom_norm_mean(m,sub),u_mse_norm_mean(m,sub),-u_mse_norm_std(m,sub),u_mse_norm_std(m,sub),-u_rom_norm_std(m,sub),u_rom_norm_std(m,sub),'LineWidth',2);hold on;
-                    e.Color = 'black';
-                    e.CapSize = 15;
-                      s = scatter(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),'filled','MarkerEdgeColor',color,'MarkerFaceColor',color);hold on;  
-                    s.SizeData = 100;
-                    s.Marker = 'h';
-                    if m == 7
-                        text(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),label(m),'color',color,'fontweight','bold','FontSize', 8);
-                    end
-                    ylim([-2 10])
-                    
-                else
-                    c = 3;
-                    if rom(m,sub) < 1.5
-                        subplot(5,3,[10 11 13 14])
-%                         colormap jet
-%                         caxis([20 51])
-                    else
-                        subplot(5,3,[12 15])
-                    end
-                    %h = breakxaxis([1.5 3]);
-                    %plot(u_rom_norm_mean(m,sub),u_mse_norm_mean(m,sub),'d','Color',[0 0 0],'LineWidth',4);hold on;
-                    e = errorbar(u_rom_norm_mean(m,sub),u_mse_norm_mean(m,sub),-u_mse_norm_std(m,sub),u_mse_norm_std(m,sub),-u_rom_norm_std(m,sub),u_rom_norm_std(m,sub),'LineWidth',2);hold on;
-                    e.Color = 'black';
-                    e.CapSize = 15;
-                       s = scatter(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),'filled','MarkerEdgeColor',color,'MarkerFaceColor',color);hold on;  
-                    s.SizeData = 100;
-                    s.Marker = 'd';
-                    if m == 11
-                        text(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),label(m),'color',color,'fontweight','bold','FontSize', 8);
-                    end
-                    ylim([-2 10])
-                    
-                end
-                
-                
-                
-                ax = gca;
-                %ax.LineWidth = 1.5;
-                ax.FontSize = 10; 
-                %xticks([0 ceil(max(max(a_times))/10)*10])
-                box off
-                set(gca,'fontweight','bold')
+        if m < 6
+            c = 1;
+            if rom(m,sub) < 1.5
+                subplot(5,3,[10 11 13 14])
+            else
+                subplot(5,3,[12 15])
+                xlim([3 5.5])
+            end
+            e = errorbar(u_rom_norm_mean(m,sub),u_mse_norm_mean(m,sub),-u_mse_norm_std(m,sub),u_mse_norm_std(m,sub),-u_rom_norm_std(m,sub),u_rom_norm_std(m,sub),'LineWidth',2);hold on;
+            e.Color = 'black';
+            e.CapSize = 15;
+            s = scatter(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),'filled','MarkerEdgeColor',color,'MarkerFaceColor',color);hold on;
+            s.SizeData = 100;
+            s.Marker = 's';
+            if m == 1
+                text(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),label(m),'color',color,'fontweight','bold','FontSize', 8);
+            end
+            ylim([-2 10])
+        elseif m > 6 && m < 10
+            c = 2;
+            if rom(m,sub) < 1.5
+                subplot(5,3,[10 11 13 14])
+            else
+                subplot(5,3,[12 15])
+            end
+            e = errorbar(u_rom_norm_mean(m,sub),u_mse_norm_mean(m,sub),-u_mse_norm_std(m,sub),u_mse_norm_std(m,sub),-u_rom_norm_std(m,sub),u_rom_norm_std(m,sub),'LineWidth',2);hold on;
+            e.Color = 'black';
+            e.CapSize = 15;
+            s = scatter(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),'filled','MarkerEdgeColor',color,'MarkerFaceColor',color);hold on;
+            s.SizeData = 100;
+            s.Marker = 'h';
+            if m == 7
+                text(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),label(m),'color',color,'fontweight','bold','FontSize', 8);
+            end
+            ylim([-2 10])
 
-                
-%                 figure(3)
-%                 if m < 6 
-%                     c = 1;
-%                     subplot(3,2,1)
-%                     s = scatter(0,(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),[],ARAT(sub),'filled');hold on;  
-%                     s.SizeData = 100;
-%                     s.Marker = 's';
-%                     %axis([-1 1 -1 8])
-%                     set(gca,'XTickLabel',[]);
-%                     set(gca,'XTickLabel',[]);
-%                     set(gca,'xTick',[]);
-%                      h = colorbar;
-%                 colormap jet
-%                 caxis([20 51])
-%                 h.Label.String = 'ARAT Score';
-%                 title('Normalized MSE Thumb MCP')
-%                 
-%                     
-%                     subplot(3,2,2)
-%                     s = scatter(rom(m,sub),0,[],ARAT(sub),'filled');hold on;  
-%                     s.SizeData = 100;
-%                     s.Marker = 's';
-%                     %axis([-1 5 -1 1])
-%                      set(gca,'YTickLabel',[]);
-%                     set(gca,'yTick',[]);
-%                             h = colorbar;
-%                 colormap jet
-%                 caxis([20 51])
-%                 h.Label.String = 'ARAT Score';
-%                 title('Normalized Std Thumb MCP')
-%                 
-%                 elseif m > 6 & m < 10  
-%                     c = 2;
-%                     subplot(3,2,3)
-%                     s = scatter(0,(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),[],ARAT(sub),'filled');hold on;
-%                     s.SizeData = 100;
-%                     s.Marker = 'h';
-%                     set(gca,'XTickLabel',[]);
-%                     
-%                      subplot(3,2,4)
-%                     s = scatter(rom(m,sub),0,[],ARAT(sub),'filled');hold on;
-%                     s.SizeData = 100;
-%                     s.Marker = 'h';
-%                     set(gca,'YTickLabel',[]);
-%                 else
-%                     c = 3;
-%                     subplot(3,2,5)
-%                     s = scatter(0,(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),[],ARAT(sub),'filled');hold on;
-%                     s.SizeData = 100;
-%                     s.Marker = 'h';
-%                     set(gca,'XTickLabel',[]);
-%                     set(gca,'xTick',[]);
-%                     
-%                      subplot(3,2,6)
-%                     s = scatter(rom(m,sub),0,[],ARAT(sub),'filled');hold on;
-%                     s.SizeData = 100;
-%                     s.Marker = 'h';
-%                     set(gca,'YTickLabel',[]);
-%                 end
-                %grid on;
-               sub1 = sub1 + 1;
-                
-                
+        else
+            c = 3;
+            if rom(m,sub) < 1.5
+                subplot(5,3,[10 11 13 14])
+            else
+                subplot(5,3,[12 15])
+            end
+            e = errorbar(u_rom_norm_mean(m,sub),u_mse_norm_mean(m,sub),-u_mse_norm_std(m,sub),u_mse_norm_std(m,sub),-u_rom_norm_std(m,sub),u_rom_norm_std(m,sub),'LineWidth',2);hold on;
+            e.Color = 'black';
+            e.CapSize = 15;
+            s = scatter(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),'filled','MarkerEdgeColor',color,'MarkerFaceColor',color);hold on;
+            s.SizeData = 100;
+            s.Marker = 'd';
+            if m == 11
+                text(rom(m,sub),(mse_a(m,sub)-mse_u(m,sub))./mse_u(m,sub),label(m),'color',color,'fontweight','bold','FontSize', 8);
+            end
+            ylim([-2 10])
+
+        end
+
+        ax = gca;
+        ax.FontSize = 10;
+        box off
+        set(gca,'fontweight','bold')
+
+
+        sub1 = sub1 + 1;
+           
     end
     sub1 = 1;
 end
-                
-                %grid on;
-%                 h = colorbar;
-%                 colormap jet
-%                 caxis([20 51])
-%                 h.Label.String = 'ARAT Score';
-                ylabel('T2TC (Norm)','fontweight','bold','FontSize',12)
-                xlabel('ROM (Norm)','fontweight','bold','FontSize',12)
-                xlim([3 5.5])
-                set(gca,'fontweight','bold')
-                %break_axis('axis','x','position',1.2)
 
-               
-%                  plot([-1.5 8],[0 0],'k--');hold on;
-%                 plot([0 0],[-2 18],'k--');hold on;
-                %axis([-1.5 8 -2 35])
-
-
-% figure(10)
-% 
-% %subplot(4,3,[7:12]);
-% 
-% %figure(4)
-% rom = rom(:,v:r);
-% mse_t = (mse_a(:,v:r)-mse_u(:,v:r))./mse_u(:,v:r);
-% 
-% X = [rom(:) mse_t(:)];
-% idx = kmeans(X,4);
-% h = gscatter(X(:,1),X(:,2),idx,'rmgb',[],30);hold on;
-% z = repmat([ones(1,5) 2.*ones(1,3) 3.*ones(1,4)],1,(v+r)-1);
-% 
-% %e.Color = 'black';
-% %e.CapSize = 15;
-% 
-% %gscatter3(X(:,1),X(:,2),z,idx,'rkgb',{'o','o','o','o'},8,'auto');
-% 
-% ylabel('Norm Precision')
-% xlabel('Norm ROM')
-% zticks([1:3])
-% zticklabels({'Distal Joints','Palm Joints','Proximal Joints'})
-% %legend('Normal','High Variance, Poor Joint Control','Extremely Poor Joint Control','Joint Overcompensation')
-% label = ["Thumb MCP","Thumb DIP","Index MCP","Index PIP","Index DIP","Elbow Flex","Palm Abd/Add","Palm Flex","Palm Sup/Pron","Shoulder Hor Flex","Shoulder Vert Flex","Shoulder Roll"];
-% plot([-1.5 9],[0 0],'k--','LineWidth',3);hold on;
-% plot([0 0],[-5 35],'k--','LineWidth',3);hold on;
-% 
-% %plot(u_rom_norm_mean,u_mse_norm_mean,'Color',[0 0 0],'LineWidth',4);hold on;
-% %e = errorbar(u_rom_norm_mean,u_mse_norm_mean,-u_mse_norm_std,u_mse_norm_std,-u_rom_norm_std,u_rom_norm_std,'k','LineWidth',2);
-% 
-% x = [-15*max(max(u_rom_norm_mean)):0.001:15*max(max(u_rom_norm_mean))];
-% 
-% %y =  sqrt(max(max(u_mse_norm_mean)).^2 .*(1 - ((x - max(max(u_rom_norm_std))).^2 ./ max(max(u_rom_norm_mean)).^2))) + max(max(u_mse_norm_std));
-% h =  0;%max(max(u_rom_norm_mean));
-% k = 0;%max(max(u_mse_norm_mean));
-% a = max(max(u_rom_norm_std)); 
-% b = max(max(u_mse_norm_std)); 
-% 
-% y = nonzeros(real(sqrt(b.^2 .* (1 - (x - h).^2 ./ a.^2)) + k));
-% 
-% x = linspace(-15*max(max(u_rom_norm_mean)),15*max(max(u_rom_norm_mean)),length(y));
-% x = max(max(u_rom_norm_mean))+[x x];
-% z = max(max(u_mse_norm_mean))+[y; -y]';
-% 
-% %plot(x,y);hold on;
-% %plot(x,-y)
-% 
-% patch(x,z,'k','FaceAlpha',.6)
-% %patch(x,-y,'r','FaceAlpha',.3)
-% 
-% % h = ellipse(max(max(u_rom_norm_std)),max(max(u_mse_norm_std)),0,max(max(u_rom_norm_mean)),max(max(u_mse_norm_mean)),'k',1000);
-% % h.FaceColor = 'blue';
-% % h.FaceAlpha = 0.5;
-% 
-% 
-% %axis([-1.5 8 -2 18])
-% % for m = 1:12
-% %     for sub = 1:4
-% %                 if m < 6 
-% %                     c = 1;
-% %                     text(rom(m,sub),mse_t(m,sub),append(label(m),' ',num2str(ARAT(sub+1))),'FontSize',8)
-% %                 elseif m > 6 & m < 10  
-% %                     c = 2;
-% %                     text(rom(m,sub),mse_t(m,sub),append(label(m),' ',num2str(ARAT(sub+1))),'FontSize',8)
-% %                 else
-% %                     c = 3;
-% %                     text(rom(m,sub),mse_t(m,sub),append(label(m),' ',num2str(ARAT(sub+1))),'FontSize',8)
-% %                 end
-% %     end
-% % end
-% 
+ylabel('T2TC (Norm)','fontweight','bold','FontSize',12)
+xlabel('ROM (Norm)','fontweight','bold','FontSize',12)
+xlim([3 5.5])
+set(gca,'fontweight','bold')
+              
