6d798e89a5790c6a8727902dd64a2c1fa6671050
preeya <pkhanna104@gmail.com>
2022-10-06 21:57:30 -0700

    simplified bootstrapping, removed u_rom_norm as output since seemed random and u_mse_norm wasnt included

--- tom_scripts/normal_mean_std.m
+++ tom_scripts/normal_mean_std.m
@@ -1,26 +1,34 @@
-function [u_rom_norm,u_rom_norm_mean,u_rom_norm_std,u_mse_norm_mean, u_mse_norm_std] = normal_mean_std(mse_rom)
+function [u_rom_norm_mean,u_rom_norm_2xstd,u_mse_norm_mean, u_mse_norm_2xstd] = normal_mean_std(mse_rom)
 
-    u_rom = mse_rom.u_rom;
-    u_mse = mse_rom.u_mse;
-  
+u_rom = mse_rom.u_rom_2_pinch; % rom calculated from start --> pinch on un-zscored data
+u_mse = mse_rom.u_mse_2_pinch; % mse calculated from start --> pinch on z-scored data
 
-    C = nchoosek([1:10],5);
-     %bootstrap without replacement unaffect ROM!  -> used for determinng
-     %"normal data
-     for m = 1:12
-            for k = 1:size(nchoosek([1:10],5),1) %1 to total # of possible permutations -> 252 here
-                u_mse_train = median(cell2mat(u_mse(C(k,:),m))); %produces a scalar
-                u_mse_test = median(setdiff(cell2mat(u_mse(:,m)),cell2mat(u_mse(C(k,:),m)))); %produces a scalar
-                u_mse_norm(k) = (u_mse_test - u_mse_train) ./ u_mse_train; %  1 x 252 vector after for loop ends
-                                
-                u_rom_train = median(cell2mat(u_rom(C(k,:),m)));
-                u_rom_test = median(setdiff(cell2mat(u_rom(:,m)),cell2mat(u_rom(C(k,:),m))));
-                u_rom_norm(k) = (u_rom_test - u_rom_train) ./ u_rom_train;
-            end      
-        u_rom_norm_mean(m) = mean(u_rom_norm);
-        u_rom_norm_std(m) = 2.*std(u_rom_norm);
-        u_mse_norm_mean(m) = mean(u_mse_norm);
-        u_mse_norm_std(m) = 2.*std(u_mse_norm);
-         
-     end
-       
+
+C = nchoosek([1:10],5);
+%bootstrap without replacement unaffect ROM!  -> used for determinng
+%"normal datas
+for m = 1:12 % for each joint
+    
+    % For each subselection:
+    for k = 1:size(nchoosek([1:10],5),1) %1 to total # of possible permutations -> 252 here
+        
+        grp1 = C(k, :);
+        grp2 = setdiff(1:10, grp1);
+        
+        u_mse_train = median(cell2mat(u_mse(grp1, m))); %produces a scalar
+        u_mse_test =  median(cell2mat(u_mse(grp2 ,m))); %produces a scalar
+        
+        u_mse_norm(k) = (u_mse_test - u_mse_train) ./ u_mse_train; %  1 x 252 vector after for loop ends
+        
+        u_rom_train = median(cell2mat(u_rom(grp1, m)));
+        u_rom_test =  median(cell2mat(u_rom(grp2, m)));
+        u_rom_norm(k) = (u_rom_test - u_rom_train) ./ u_rom_train;
+    end
+    
+    
+    u_rom_norm_mean(m) = mean(u_rom_norm);
+    u_rom_norm_2xstd(m) = 2.*std(u_rom_norm);
+    u_mse_norm_mean(m) = mean(u_mse_norm);
+    u_mse_norm_2xstd(m) = 2.*std(u_mse_norm);
+    
+end
