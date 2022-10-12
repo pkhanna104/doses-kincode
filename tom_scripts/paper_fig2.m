8bc04c06396cdc6fda9aa82de2c57512b710eeaa
toppenheim1 <113718090+toppenheim1@users.noreply.github.com>
2022-10-12 14:10:37 -0700

    Add files via upload

--- tom_scripts/paper_fig2.m
+++ tom_scripts/paper_fig2.m
@@ -10,17 +10,17 @@ else % Tom computer
 end
 
 %Add subjects here if needed..
-subject = ["B8M","S13J_small","B12J"];
+subject = ["B8M","S13J_small","B12J","C9K","W16H_small","R15J_small"];
 
 norms.a_rom_norm_2_pinch = [];
 norms.a_mse_norm_2_pinch = [];
 norms.u_mse_norm_mean = [];
-norms.u_mse_norm_2xstd = [];
+norms.u_mse_norm_std = [];
 norms.u_rom_norm_mean = [];
-norms.u_rom_norm_2xstd = [];
+norms.u_rom_norm_std = [];
 %% Plotting paper figure 2A-C
 
-for sub = 1:size(subject,2)
+for sub = 1:6
       if contains(subject(sub), 'ctrl')
         tk =  'controls';
     else 
@@ -34,11 +34,16 @@ for sub = 1:size(subject,2)
     
     Filename2 = string(strcat(path_to_data,'data',slash,'tom_data',slash,'jt_angle_data',slash,input,'_jt_angle_un.mat'));
     load ([Filename2]);
-    paper_fig2_raw(Filename,'un',angle_struct,sub,'off');hold on; 
-
+    if any(sub == [1:3])
+        paper_fig2_raw(Filename,'un',angle_struct,sub,'off');hold on; 
+    end
+    
     Filename2 = string(strcat(path_to_data,'data',slash,'tom_data',slash,'jt_angle_data',slash,input,'_jt_angle_aff.mat'));
     load ([Filename2]);
-    paper_fig2_raw(Filename,'aff',angle_struct,sub,'off');hold on; 
+    if any(sub == [1:3])
+        paper_fig2_raw(Filename,'aff',angle_struct,sub,'off');hold on; 
+    end
+    
 
     if contains(input,'_small') 
         input = erase(input,'_small');
@@ -54,15 +59,15 @@ for sub = 1:size(subject,2)
     norms.a_rom_norm_2_pinch = horzcat(norms.a_rom_norm_2_pinch,a_rom_norm_2_pinch');
     norms.a_mse_norm_2_pinch = horzcat(norms.a_mse_norm_2_pinch,a_mse_norm_2_pinch');
     norms.u_mse_norm_mean = horzcat(norms.u_mse_norm_mean,u_mse_norm_mean');
-    norms.u_mse_norm_std = horzcat(norms.u_mse_norm_2xstd,u_mse_norm_2xstd');
+    norms.u_mse_norm_std = horzcat(norms.u_mse_norm_std,u_mse_norm_2xstd');
 
     norms.u_rom_norm_mean = horzcat(norms.u_rom_norm_mean,u_rom_norm_mean');
-    norms.u_rom_norm_std = horzcat(norms.u_rom_norm_2xstd,u_rom_norm_2xstd');
+    norms.u_rom_norm_std = horzcat(norms.u_rom_norm_std,u_rom_norm_2xstd');
 
 end
 
 
-%% Plot Paper Figure 2D-F find  indices of max mse and std omitting palm and shoulder roll 
+%% Plot Paper Figure 2D-G find  indices of max mse and std omitting palm and shoulder roll 
 % ->  used for creatting max error bar in paper_fig2_norms
  
 joints = setdiff([1:12],[9 12]);
@@ -72,6 +77,7 @@ joints = setdiff([1:12],[9 12]);
 %find indices of max rom std omitting palm and shoulder roll
 [row.rom column.rom] = find(norms.u_rom_norm_std == max(norms.u_rom_norm_std(joints,:),[],'all')); %location of largest rom std of all subjects
 
-paper_fig2_norms(norms,row,column);
+paper_fig2D_G(norms,row,column);
+paper_fig2H(norms,row,column);
 
               
