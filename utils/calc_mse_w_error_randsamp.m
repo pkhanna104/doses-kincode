0b876e4d58e81a1623731639216e33fdc5c5571f
preeya <pkhanna104@gmail.com>
2022-10-20 10:08:13 -0700

    added method to estimate MSE error with random sampling

---
+++ utils/calc_mse_w_error_randsamp.m
@@ -0,0 +1,23 @@
+function [mse_min, mse_max, mse_true] = calc_mse_w_error_randsamp(jt, med, std_err)
+
+assert(length(jt) == length(med))
+N = length(jt); 
+
+ 
+mse_true = sqrt(1/N.*sum((jt - med).^2)); 
+mse_rand = []; 
+for i = 1:1000
+    % Add randomly sampled noise to the joint angle 
+    jt_rand = jt + (std_err*randn(size(jt))); 
+
+    mse_ = sqrt(1/N.*sum((jt_rand - med).^2)); 
+    mse_rand = [mse_rand, mse_]; 
+end
+
+mse_std = std(mse_rand); 
+mse_min = mse_true - mse_std; 
+mse_max = mse_true + mse_std; 
+
+
+% MSE equation 
+%sqrt(1/length(trl_s2p).*sum(diff_.^2));
