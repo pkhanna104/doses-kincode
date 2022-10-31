6d5f074bc4eef1ca08593e0252396e28cc18189c
preeya <pkhanna104@gmail.com>
2022-10-31 12:58:15 -0700

    added diff options for diameter of confidence ellipse

--- utils/plot_conf_ellipse.m
+++ utils/plot_conf_ellipse.m
@@ -1,4 +1,11 @@
-function test_result = plot_conf_ellipse(ax, x_og, y_og, col, test_x, test_y)
+function test_result = plot_conf_ellipse(ax, x_og, y_og, col, test_x, test_y, ...
+    perc_confidence)
+
+ if ~exist('perc_confidence','var')
+     % perc_confidence does not exist, so default it to something
+      perc_confidence = 95;
+ end
+
 
 % Make sure column matrices
 assert(size(x_og, 2) == 1)
@@ -12,8 +19,10 @@ Y0 = mean(y_og);
 x = x_og - X0; 
 y = y_og - Y0; 
 
-test_x = test_x - X0; 
-test_y = test_y - Y0; 
+if ~isempty(test_x)
+    test_x = test_x - X0; 
+    test_y = test_y - Y0; 
+end
 
 % Get out orientation radius;
 loadings = PCA([x, y]); 
@@ -29,9 +38,19 @@ R = [cos(theta) -sin(theta);
 X_unrot = (R*[x, y]')'; 
 
 %https://www.visiondummy.com/2014/04/draw-error-ellipse-representing-covariance-matrix/
-x_dem = std(X_unrot(:, 1))*sqrt(5.991); 
-y_dem = std(X_unrot(:, 2))*sqrt(5.991); 
-
+if perc_confidence == 95
+    x_dem = std(X_unrot(:, 1))*sqrt(5.991); 
+    y_dem = std(X_unrot(:, 2))*sqrt(5.991); 
+    linestyle = '-';
+elseif perc_confidence == 90
+    x_dem = std(X_unrot(:, 1))*sqrt(4.605); 
+    y_dem = std(X_unrot(:, 2))*sqrt(4.605);
+    linestyle = '--';
+elseif perc_confidence == 75
+    x_dem = std(X_unrot(:, 1))*sqrt(2.77); 
+    y_dem = std(X_unrot(:, 2))*sqrt(2.77);
+    linestyle = '--';
+end
 % Plot the ellipse
 x_e_opts = -x_dem:.01:x_dem; 
 
@@ -55,22 +74,26 @@ R_e(:, 1) = R_e(:, 1) + X0;
 R_e(:, 2) = R_e(:, 2) + Y0;
 
 % Plot the coordinates 
-plot(ax, R_e(:,1), R_e(:,2), '-', 'Color', col);
+plot(ax, R_e(:,1), R_e(:,2), linestyle, 'Color', col);
 
 % Test out which points are inside vs outside the ellipse
 
 % Rotate points: 
-X_R_test = (R*[test_x test_y]')';
-
-test_result = []; 
-N = length(test_x); 
-
-for n = 1:N
-    if ((X_R_test(n, 1)/x_dem).^2 + (X_R_test(n, 2)/y_dem).^2) <= 1
-        test_result = [test_result 0]; 
-    else
-        test_result = [test_result 1]; 
+if ~isempty(test_x)
+    X_R_test = (R*[test_x test_y]')';
+    
+    test_result = []; 
+    N = length(test_x); 
+    
+    for n = 1:N
+        if ((X_R_test(n, 1)/x_dem).^2 + (X_R_test(n, 2)/y_dem).^2) <= 1
+            test_result = [test_result 0]; 
+        else
+            test_result = [test_result 1]; 
+        end
     end
+else
+    test_result = []; 
 end
 
 end
